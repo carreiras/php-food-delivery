354c3acc186f9becd194d8d1b2fc5ec0e0911738
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-03-18 08:58:26 -0300

    Trabalhando com validação de formulários

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -15,6 +15,30 @@ class UsuarioModel extends Model
     protected $updatedField = 'atualizado_em';
     protected $deletedField = 'deletado_em';
 
+    protected $validationRules = [
+        'nome' => 'required|min_length[4]|max_length[120]',
+        'email' => 'required|valid_email|is_unique[usuarios.email]',
+        'cpf' => 'required|exact_length[14]|is_unique[usuarios.cpf]',
+        'password' => 'required|min_length[6]',
+        'password_confirmation' => 'required_with[password]|matches[password]',
+    ];
+
+    protected $validationMessages = [
+        'nome' => [
+            'required' => 'Este campo é obrigatório.',
+            'min_length' => 'O tamaho mínimo é de 4 caracteres.',
+            'max_length' => 'O tamaho máximo é de 120 caracteres.',
+        ],
+        'email' => [
+            'required' => 'Este campo é obrigatório.',
+            'is_unique' => 'Este e-mail já existe.',
+        ],
+        'cpf' => [
+            'required' => 'Este campo é obrigatório.',
+            'is_unique' => 'Este CPF já existe.',
+        ],
+    ];
+
     public function procurar($term)
     {
         if ($term == null) {
